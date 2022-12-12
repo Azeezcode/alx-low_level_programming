1a04eb3cf17fd2cbe842520e848f98038cf4ce4f
azeezcode <abdulazeezadetimehin@gmail.com>
2022-12-12 01:39:20 -0800

    printing lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+ * main - Entry point
+ * Description - Program to print alphabet letters followed by a new line
+ *
+ * Return: Return 0 (success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z' ; letter++)
+	
+	
+	{
+	letter = tolower(letter);
+	putchar(letter);
+	
+	}
+	putchar ('\n');
+	return (0);
+
+}
