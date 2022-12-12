02e69980476bd382386a247beb5b5560049acf49
azeezcode <abdulazeezadetimehin@gmail.com>
2022-12-12 02:20:42 -0800

    printing lower case alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,13 @@
 int main(void)
 {
 	int letter;
-	for (letter = 'a'; letter <= 'z' ; letter++)
+	
+	for (letter ='a'; letter <='z' ; letter++)
+	
 	{
-letter = tolower(letter);
-putchar(letter);
+	
+		letter = tolower(letter);
+		putchar(letter);
 	}
 putchar ('\n');
 return (0);
