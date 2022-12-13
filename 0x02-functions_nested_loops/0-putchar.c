165e7a41be4ec75b35d145413b3b9ec481504b92
azeezcode <abdulazeezadetimehin@gmail.com>
2022-12-13 13:58:21 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - function that prints _putchar as output
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return(0);
+}
