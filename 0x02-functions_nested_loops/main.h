6c51a4420455e24f6d0189bd69802451a4eaf58a
azeezcode <abdulazeezadetimehin@gmail.com>
2022-12-13 14:24:16 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+
+/**
+ * File: main.h
+ * Author: Adetimehin AbdulAzeez
+ * Description: Header file containing Function declearations
+ */
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void time_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n,int m);
+int print_last_digit(int n);
