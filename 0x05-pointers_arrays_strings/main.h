360b34989930eb0b261eb7b902b81cdd22a9d889
azeezcode <abdulazeezadetimehin@gmail.com>
2022-12-18 23:26:37 -0800

    using pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * char prototypes()
+ * int prototypes()
+ */
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int_strlen(char *s);
-void puts(char *str);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
-int *_strcopy(char *dest, char *src);
+int *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+void swap_char(char *a, char *b);
+void puts_half(char *str);
+#endif /*MAIN_H*/
